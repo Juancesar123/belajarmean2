920ff30e062f25989d98b0cd957b6077748bca52
juancesar123 <juancesarandrianto@gmail.com>
2016-11-18 07:56:40 +0700

    CRUD MEAN
    crud mean

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ var db = mongojs("belajarmean",["bukutamu"]);
 app.set('view engine', 'jade');
 app.use(express.static(__dirname + '/views'));
 app.use('/public',express.static(__dirname + '/public'));
+app.use('/node_modules',express.static(__dirname + '/node_modules'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
@@ -15,6 +16,27 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
 router.post("/simpan_data",function(req,res){
 	db.bukutamu.insert(req.body,function(){
 		res.end();
+	})
+})
+router.get("/ambil_bukutamu",function(req,res){
+	db.bukutamu.find(function(err,docs){
+		res.json(docs);
+	})
+})
+router.post("/ubah_data",function(req,res){
+
+	db.bukutamu.update({_id:mongojs.ObjectId(req.body.id)},{$set:{
+		nama:req.body.nama,
+		jeniskelamin:req.body.jeniskelamin,
+		alamat:req.body.alamat,
+		nomortelpon:req.body.nomortelpon
+	}},function(err,docs){
+		res.end();
+	});
+});
+router.post("/hapus_data",function(req,res){
+	db.bukutamu.remove({_id:mongojs.ObjectId(req.body.id)},function(err,docs){
+		res.end()
 	})
 })
 app.use('/',router);
